f588ffd6f15506dac54b431edc0dcabdb2ca8006
SlavikKhrapach <hrapachslavik@gmail.com>
2023-05-11 03:12:34 -0400

    Created the index page and set up the fat-free

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Slavik Khrapach
+ * 5/11/2023
+ * 328/PhotoQuest/index.php
+ * Controller for howdy project
+ *
+ */
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an F3 (Fat-Free Framework) object
+$f3 = Base::instance();
+
+// Define a default route
+$f3->route('GET /', function() {
+
+    $view = new Template();
+    echo $view->render('views/info.html');
+
+});
+
+// Run the Fat-Free
+$f3->run();
