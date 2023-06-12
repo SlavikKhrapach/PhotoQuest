8b352768ec4de423707acf8f7af8f3219deb0bef
SlavikKhrapach <hrapachslavik@gmail.com>
2023-06-12 18:14:07 -0400

    Set up controller routing

---
+++ controllers/controller.php
@@ -0,0 +1,68 @@
+<?php
+
+class Controller
+{
+    private $_f3;
+
+    function __construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+
+    //F3 object
+
+    function home()
+    {
+        // Display a view page
+        $view = new Template();
+        echo $view->render('views/Home.html');
+    }
+
+    function currentQuests()
+    {
+        $view = new Template();
+        echo $view->render('views/CurrentQuests.html');
+    }
+    function pastQuests()
+    {
+        $view = new Template();
+        echo $view->render('views/PastQuests.html');
+    }
+
+    function aboutUs()
+    {
+        $view = new Template();
+        echo $view->render('views/AboutUs.html');
+    }
+
+    function contactUs()
+    {
+        $view = new Template();
+        echo $view->render('views/ContactUs.html');
+    }
+
+    function logIn($f3)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $f3->reroute('/');
+        }
+        $view = new Template();
+        echo $view->render('views/Login.html');
+    }
+
+    function logUp($f3)
+    {
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $f3->reroute('/');
+        }
+        $view = new Template();
+        echo $view->render('views/NewAccount.html');
+    }
+
+    function questTester()
+    {
+        $view = new Template();
+        echo $view->render('views/Quest.html');
+    }
+}
