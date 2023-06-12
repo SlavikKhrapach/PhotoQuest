dd5ab9974b3ef64b334df04187d55d9ec1e730f9
SlavikKhrapach <hrapachslavik@gmail.com>
2023-06-12 18:43:18 -0400

    Fixed script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,46 +1,38 @@
-var main = document.querySelector('.intro');
-var nav = document.querySelector('.nav');
-var topnav = document.querySelector('.top-nav');
+let main = document.querySelector('.intro');
+let nav = document.querySelector('.nav');
+let topnav = document.querySelector('.top-nav');
 
-// Check if the previous page was scrolled to the top
-var isScrolledToTop = localStorage.getItem('isScrolledToTop') === 'true';
+window.addEventListener('load', function() {
+    let targetDiv = document.getElementById('content');
+    let offset = targetDiv.offsetTop - 100;
 
-// Attach event listener to all anchor tags on the page
-var links = document.querySelectorAll('a');
-
-links.forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        // Check if the previous page was scrolled to the top
-        if (isScrolledToTop) {
-            // If scrolled to top, smoothly scroll to the selected section
-            event.preventDefault();
-
-            var targetSection = link.getAttribute('data-section');
-            var targetElement = document.getElementById('content');
-            var offset = targetElement.offsetTop;
-
-            window.scrollTo({
-                top: offset,
-                behavior: 'smooth'
-            });
-
-            // Wait for the scroll animation to finish
-            setTimeout(function() {
-                // Navigate to the new page
-                window.location.href = link.href;
-            }, 1000); // Adjust the delay as needed
-        } else {
-            // If not scrolled to top, load the new page instantly scrolled to the selected section
-            // The default behavior of the link will be used
-            localStorage.setItem('isScrolledToTop', 'false');
-        }
+    // Smooth scroll animation
+    window.scrollTo({
+        top: offset,
+        behavior: 'smooth'
     });
 });
 
-// Save the scroll position of the current page
-window.addEventListener('beforeunload', function() {
-    var isAtTop = window.scrollY === 0;
-    localStorage.setItem('isScrolledToTop', isAtTop ? 'true' : 'false');
+// Attach event listener to all anchor tags on the page
+let links = document.querySelectorAll('.nav a');
+
+links.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        // Prevent the default click behavior
+        event.preventDefault();
+
+        // Scroll to the top of the page
+        window.scrollTo({
+            top: 0,
+            behavior: 'auto'
+        });
+
+        // Wait for the scroll animation to finish
+        setTimeout(function() {
+            // Navigate to the new page
+            window.location.href = link.href;
+        }, 1000); // Adjust the delay as needed
+    });
 });
 
 
